2e8ac81c6f60c2a9a63d6bc33cd31535753f4868
lengushuru charles <charleslengushuru@gmail.com>
2023-03-30 11:02:22 +0300

    Fix linter errors.

--- src/components/Calculator.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Quote from './Quote';
-
-it('should render when there is quote', () => {
-  const quote = renderer.create(<Quote />).toJSON();
-  expect(quote).toMatchSnapshot();
-});
